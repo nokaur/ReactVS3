a075065589651d9400ee0abfc333291508e2dd84
nokaur <71750548+nokaur@users.noreply.github.com>
2020-09-23 12:39:36 +0530

    23Sept

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,15 @@ function greetperson(name) {
   var greet;
 }
 greetperson("Novel");
+
+var a = 1;
+var b = 2;
+if (a === 1) {
+  var a = 2;
+  let b = 3;
+  console.log(a);
+  console.log(b);
+}
+
+console.log(a);
+console.log(b);
