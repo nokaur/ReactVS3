5d32272ae323a70ea7f426b1205375305c793ad6
nokaur <71750548+nokaur@users.noreply.github.com>
2020-09-23 11:59:35 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function greetperson(name) {
+  if (name === "Novel") {
+    greet = "Hello Novel";
+  } else {
+    greet = "Hello";
+  }
+  console.log(greet);
+  var greet;
+}
+greetperson("Novel");
